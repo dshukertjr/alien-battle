e0f0694efa3c4887691a7e57c9fcd3de1e3b6d0f
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2022-08-13 15:36:45 +0900

    feat: created basic eating game

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'dart:math';
+
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+
+const _poisonHitCountToLose = 3;
 
 void main() {
   runApp(const MyApp());
@@ -22,8 +27,25 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyGame extends FlameGame with TapDetector {
+class MyGame extends FlameGame with TapDetector, HasCollisionDetection {
+  MyGame({
+    required this.onGameOver,
+    required this.onScoreUpdate,
+    required this.onPoisonHit,
+  });
+  final void Function() onGameOver;
+  final void Function(int) onScoreUpdate;
+  final void Function(int) onPoisonHit;
+
   late final MyPlayer player;
+  final random = Random();
+
+  bool _isGameOver = false;
+
+  int score = 0;
+
+  int poisonHitCount = 0;
+
   @override
   Future<void>? onLoad() async {
     await super.onLoad();
@@ -31,6 +53,12 @@ class MyGame extends FlameGame with TapDetector {
       ..width = 100
       ..height = 100;
     add(player);
+
+    for (var i = 0; i < 30; i++) {
+      _createNewCircle();
+    }
+    _keepAddingCircles();
+    _keepAddingPoisonCircles();
   }
 
   @override
@@ -38,24 +66,90 @@ class MyGame extends FlameGame with TapDetector {
     player.jump();
     super.onTap();
   }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    if (_isGameOver) {
+      return;
+    }
+    for (final child in children) {
+      if (child is PoinsonCircle) {
+        if (child.hasBeenEaten) {
+          poisonHitCount++;
+          onPoisonHit(poisonHitCount);
+          if (poisonHitCount >= _poisonHitCountToLose) {
+            _isGameOver = true;
+            onGameOver();
+          }
+        }
+      } else if (child is CircleComponent) {
+        if (child.hasBeenEaten) {
+          score++;
+          onScoreUpdate(score);
+        }
+      }
+    }
+  }
+
+  void reset() {
+    _isGameOver = false;
+    score = 0;
+    poisonHitCount = 0;
+    for (final child in children) {
+      if (child is MyPlayer) {
+        child.position = child.initialPosition;
+      } else if (child is CircleComponent) {
+        child.removeFromParent();
+      }
+    }
+  }
+
+  void _createNewCircle([bool isPoison = false]) async {
+    final randomX = random.nextDouble() * size.x * 5 + size.x;
+    final randomY = random.nextDouble() * size.y;
+    add(
+      (isPoison ? PoinsonCircle() : CircleComponent())
+        ..width = 30
+        ..height = 30
+        ..x = randomX
+        ..y = randomY,
+    );
+  }
+
+  Future<void> _keepAddingCircles() async {
+    await Future.delayed(const Duration(milliseconds: 500));
+    _createNewCircle();
+    _keepAddingCircles();
+  }
+
+  Future<void> _keepAddingPoisonCircles() async {
+    await Future.delayed(const Duration(milliseconds: 1500));
+    _createNewCircle(true);
+    _keepAddingPoisonCircles();
+  }
 }
 
-class MyPlayer extends PositionComponent with HasGameRef {
+class MyPlayer extends PositionComponent with HasGameRef, CollisionCallbacks {
   static Vector2 gravity = Vector2(0, 1000);
 
   Vector2 velocity = Vector2.zero();
 
+  late final Vector2 initialPosition;
+
   @override
   Future<void>? onLoad() async {
     await super.onLoad();
-    anchor = Anchor.bottomCenter;
-    position = gameRef.size / 2;
+    anchor = Anchor.bottomLeft;
+    initialPosition = Vector2(0, gameRef.size.y);
+    position = initialPosition;
+
+    add(RectangleHitbox());
   }
 
   @override
   void render(Canvas canvas) {
     super.render(canvas);
-
     canvas.drawRect(size.toRect(), Paint()..color = Colors.green);
   }
 
@@ -66,13 +160,62 @@ class MyPlayer extends PositionComponent with HasGameRef {
     position += velocity * dt + gravity * dt * dt / 2;
     velocity += gravity * dt;
     if (position.y > gameRef.size.y) {
-      position = Vector2(gameRef.size.x / 2, gameRef.size.y);
+      position = initialPosition;
+
       velocity = Vector2(0, 0);
     }
   }
 
   void jump() {
     velocity = Vector2(0, -600);
+  }
+}
+
+class CircleComponent extends PositionComponent
+    with CollisionCallbacks, HasGameRef {
+  static final velocity = Vector2(-100, 0);
+
+  bool hasBeenEaten = false;
+
+  @override
+  Future<void>? onLoad() async {
+    await super.onLoad();
+    anchor = Anchor.center;
+
+    add(CircleHitbox());
+  }
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+    canvas.drawCircle(Offset.zero, size.x / 2, Paint()..color = Colors.red);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    position += velocity * dt;
+
+    if (position.x < 0) {
+      removeFromParent();
+    }
+  }
+
+  @override
+  void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
+    super.onCollision(intersectionPoints, other);
+    if (other is MyPlayer) {
+      hasBeenEaten = true;
+      removeFromParent();
+    }
+  }
+}
+
+class PoinsonCircle extends CircleComponent {
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+    canvas.drawCircle(Offset.zero, size.x / 2, Paint()..color = Colors.blue);
   }
 }
 
@@ -84,10 +227,79 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final game = MyGame();
+  late final MyGame game;
+
+  @override
+  void initState() {
+    super.initState();
+    game = MyGame(
+      onGameOver: () {
+        showDialog(
+            barrierDismissible: false,
+            context: context,
+            builder: ((context) {
+              return AlertDialog(
+                title: const Text('Game Over'),
+                actions: [
+                  TextButton(
+                      onPressed: () {
+                        game.reset();
+                        setState(() {});
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('Retry'))
+                ],
+              );
+            }));
+      },
+      onScoreUpdate: (newScore) {
+        setState(() {});
+      },
+      onPoisonHit: (newPoisonHitCount) {
+        setState(() {});
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return GameWidget(game: game);
+    return Scaffold(
+      body: Stack(
+        children: [
+          GameWidget(game: game),
+          Positioned(
+            top: MediaQuery.of(context).padding.top + 10,
+            right: 10,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  '${game.score} points',
+                  style: const TextStyle(color: Colors.white),
+                ),
+                Row(
+                  children: List.generate(_poisonHitCountToLose, (index) {
+                    final lifeLeft =
+                        _poisonHitCountToLose - game.poisonHitCount;
+                    if (index < lifeLeft) {
+                      return const Icon(
+                        Icons.favorite,
+                        color: Colors.pink,
+                      );
+                    } else {
+                      return const Icon(
+                        Icons.favorite_border,
+                        color: Colors.grey,
+                      );
+                    }
+                  }),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
