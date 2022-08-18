d667a547e54966dd55530c250763ede532727dc4
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2022-08-18 13:28:38 +0900

    feat: can release colored balls

--- lib/game/alien.dart
+++ lib/game/alien.dart
@@ -1,165 +1,220 @@
-import 'package:flame/collisions.dart';
-import 'package:flame/components.dart';
-import 'package:flame/flame.dart';
+import 'package:flame/palette.dart';
+import 'package:flame_forge2d/flame_forge2d.dart';
 import 'package:flutter/material.dart';
 import 'package:multiplayer/utils/constants.dart';
 
-class Alien extends PositionComponent with HasGameRef, CollisionCallbacks {
+// class Alien extends PositionComponent with HasGameRef, CollisionCallbacks {
+//   final int playerIndex;
+//   Alien({
+//     required this.isMine,
+//     required this.playerIndex,
+//     required this.onHpChange,
+//   });
+
+//   final void Function() onHpChange;
+//   final bool isMine;
+
+//   static const _receiveDamageDuration = Duration(milliseconds: 300);
+
+//   String get getImagePath {
+//     return 'alien$playerIndex.png';
+//   }
+
+//   Vector2 velocity = Vector2.zero();
+
+//   /// How fast the velocity decreases per second
+//   static const _deceleration = -150.0;
+
+//   late final Sprite sprite;
+
+//   bool isAttacking = false;
+//   double healthPoints = initialHealthPoints;
+
+//   /// `true` when receiving damage.
+//   /// Won't receive any damage while true.
+//   /// Will turn false after _receiveDamageDuration
+//   bool receivingDamage = false;
+
+//   @override
+//   Future<void>? onLoad() async {
+//     await super.onLoad();
+//     final image = await Flame.images.load(getImagePath);
+
+//     width = 70;
+//     height = 70;
+
+//     sprite = Sprite(image);
+
+//     anchor = Anchor.topLeft;
+
+//     add(CircleHitbox(anchor: Anchor.center));
+//   }
+
+//   @override
+//   void render(Canvas canvas) {
+//     super.render(canvas);
+//     sprite.render(canvas,
+//         size: size, position: Vector2.zero(), anchor: Anchor.center);
+//   }
+
+//   @override
+//   void update(double dt) {
+//     super.update(dt);
+//     position += velocity * dt;
+//     final unit = velocity.normalized();
+//     velocity += unit * _deceleration * dt;
+//     if (velocity.length < 1) {
+//       velocity.setZero();
+//       isAttacking = false;
+//     }
+//   }
+
+//   @override
+//   void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
+//     super.onCollision(intersectionPoints, other);
+//     if (other is ScreenHitbox) {
+//       final reflectionVector =
+//           intersectionPoints.first.x == intersectionPoints.last.x
+//               ? Vector2(1, 0)
+//               : Vector2(0, 1);
+//       velocity.reflect(reflectionVector);
+//     } else if (other is Alien) {
+//       if (velocity.isZero() && other.velocity.isZero()) {
+//         return;
+//       }
+//       // final isAttacker = velocity.length > other.velocity.length;
+//       // if (isAttacker) {
+//       // } else {
+//       //   if (!receivingDamage &&
+//       //       other.isAttacking &&
+//       //       other.velocity.length > 10) {
+//       //     receivingDamage = true;
+//       //     healthPoints -= 40;
+//       //     if (healthPoints <= 0) {
+//       //       removeFromParent();
+//       //     }
+//       //     onHpChange();
+//       //     Future.delayed(_receiveDamageDuration)
+//       //         .then((value) => receivingDamage = false);
+//       //   }
+
+//       // final velocityMagnitude = (other.velocity.x - other.velocity.y) /
+//       //     (reflectionVector.x - reflectionVector.y);
+//       // velocity = reflectionVector * velocityMagnitude;
+//       // }
+//     }
+//   }
+
+//   /// Released the alien to move in certain direction
+//   void release(Vector2 releaseVelocity) {
+//     velocity = releaseVelocity;
+//     isAttacking = true;
+//   }
+// }
+
+class Alien extends BodyComponent with ContactCallbacks {
+  late Paint originalPaint;
+  final bool _giveNudge = false;
+
+  double healthPoints = initialHealthPoints;
+
+  final Paint _blue = BasicPalette.blue.paint();
+  final bool isMine;
   final int playerIndex;
+  final void Function() onHpChange;
+
   Alien({
     required this.isMine,
     required this.playerIndex,
     required this.onHpChange,
-  });
-
-  final void Function() onHpChange;
-  final bool isMine;
-
-  static const _receiveDamageDuration = Duration(milliseconds: 300);
+  }) {
+    originalPaint = randomPaint();
+    paint = originalPaint;
+  }
 
   String get getImagePath {
     return 'alien$playerIndex.png';
   }
 
-  Vector2 velocity = Vector2.zero();
+  Paint randomPaint() => PaintExtension.random(withAlpha: 0.9, base: 100);
 
-  /// How fast the velocity decreases per second
-  static const _deceleration = -150.0;
+  // @override
+  // Future<void>? onLoad() async {
+  //   super.onLoad();
+  //   final image = await Flame.images.load(getImagePath);
 
-  late final Sprite sprite;
+  //   width = 70;
+  //   height = 70;
 
-  bool isAttacking = false;
-  double healthPoints = initialHealthPoints;
+  //   position = _position;
 
-  /// `true` when receiving damage.
-  /// Won't receive any damage while true.
-  /// Will turn false after _receiveDamageDuration
-  bool receivingDamage = false;
+  //   sprite = Sprite(image);
+  // }
 
-  @override
-  Future<void>? onLoad() async {
-    await super.onLoad();
-    final image = await Flame.images.load(getImagePath);
-
-    width = 70;
-    height = 70;
-
-    sprite = Sprite(image);
-
-    anchor = Anchor.topLeft;
-
+  Vector2 get _getInitialPosition {
     final sixthOfField = gameRef.size.x / 6;
     switch (playerIndex) {
       case 0:
-        position = Vector2(sixthOfField * 1, sixthOfField);
-        break;
+        return Vector2(sixthOfField * 1, sixthOfField);
+
       case 1:
-        position = Vector2(sixthOfField * 5, sixthOfField);
-        break;
+        return Vector2(sixthOfField * 5, sixthOfField);
+
       case 2:
-        position = Vector2(sixthOfField * 1, sixthOfField * 5);
-        break;
+        return Vector2(sixthOfField * 1, sixthOfField * 5);
+
       case 3:
-        position = Vector2(sixthOfField * 5, sixthOfField * 5);
-        break;
+        return Vector2(sixthOfField * 5, sixthOfField * 5);
+
       case 4:
-        position = Vector2(sixthOfField * 2, sixthOfField * 3);
-        break;
+        return Vector2(sixthOfField * 2, sixthOfField * 3);
+
       case 5:
-        position = Vector2(sixthOfField * 4, sixthOfField * 3);
-        break;
-    }
-
-    add(CircleHitbox(anchor: Anchor.center));
-  }
-
-  @override
-  void render(Canvas canvas) {
-    super.render(canvas);
-    sprite.render(canvas,
-        size: size, position: Vector2.zero(), anchor: Anchor.center);
-  }
-
-  @override
-  void update(double dt) {
-    super.update(dt);
-    position += velocity * dt;
-    final unit = velocity.normalized();
-    velocity += unit * _deceleration * dt;
-    if (velocity.length < 1) {
-      velocity.setZero();
-      isAttacking = false;
+        return Vector2(sixthOfField * 4, sixthOfField * 3);
+      default:
+        return Vector2(sixthOfField * 4, sixthOfField * 3);
     }
   }
 
   @override
-  void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
-    super.onCollision(intersectionPoints, other);
-    if (other is ScreenHitbox) {
-      final reflectionVector =
-          intersectionPoints.first.x == intersectionPoints.last.x
-              ? Vector2(1, 0)
-              : Vector2(0, 1);
-      velocity.reflect(reflectionVector);
-    } else if (other is Alien) {
-      if (velocity.isZero() && other.velocity.isZero()) {
-        return;
-      }
-      final reflectionVector =
-          (intersectionPoints.first - intersectionPoints.last).normalized();
+  Body createBody() {
+    final shape = CircleShape();
+    shape.radius = 3;
 
-      if (other.velocity.isZero()) {
-        final velocityMagnitude = (other.velocity.x - other.velocity.y) /
-            (reflectionVector.x - reflectionVector.y);
-        final velocityOutMagnitude =
-            other.velocity.x - velocityMagnitude * reflectionVector.x;
-        velocity = velocity.normalized().reflected(reflectionVector) *
-            velocityOutMagnitude;
-      } else if (velocity.isZero()) {
-        final velocityMagnitude = (other.velocity.x - other.velocity.y) /
-            (reflectionVector.x - reflectionVector.y);
-        // final velocityOutMagnitude =
-        //     other.velocity.x - velocityMagnitude * reflectionVector.x;
-        velocity = reflectionVector.normalized() * velocityMagnitude;
-      } else {
-        // final n1 = velocity.reflected(reflectionVector).normalized();
-        // final n2 = other.velocity.reflected(reflectionVector).normalized();
+    final fixtureDef = FixtureDef(
+      shape,
+      restitution: 0,
+      density: 1,
+      friction: 0,
+    );
 
-        // final velocityMagnitude = (n2.x * (velocity.y + other.velocity.y) -
-        //         n2.y * (velocity.x + other.velocity.x)) /
-        //     (n2.x + n1.y - n1.x * n2.y);
-        // final otherVelocityMagnitude =
-        //     (velocity.x + other.velocity.x - n1.x * velocityMagnitude) / n2.x;
-        // velocity = velocity.reflected(reflectionVector).normalized() *
-        //     velocityMagnitude;
-      }
+    final bodyDef = BodyDef(
+      userData: this,
+      linearDamping: 0.2,
+      position: _getInitialPosition,
+      type: BodyType.dynamic,
+    );
 
-      // final isAttacker = velocity.length > other.velocity.length;
-      // if (isAttacker) {
-      // } else {
-      //   if (!receivingDamage &&
-      //       other.isAttacking &&
-      //       other.velocity.length > 10) {
-      //     receivingDamage = true;
-      //     healthPoints -= 40;
-      //     if (healthPoints <= 0) {
-      //       removeFromParent();
-      //     }
-      //     onHpChange();
-      //     Future.delayed(_receiveDamageDuration)
-      //         .then((value) => receivingDamage = false);
-      //   }
+    return world.createBody(bodyDef)..createFixture(fixtureDef);
+  }
 
-      // final velocityMagnitude = (other.velocity.x - other.velocity.y) /
-      //     (reflectionVector.x - reflectionVector.y);
-      // velocity = reflectionVector * velocityMagnitude;
-      // }
+  @override
+  void beginContact(Object other, Contact contact) {
+    super.beginContact(other, contact);
+    if (other is Alien) {
+      print('here');
     }
+  }
+
+  @override
+  void renderCircle(Canvas c, Offset center, double radius) {
+    super.renderCircle(c, center, radius);
+    final lineRotation = Offset(0, radius);
+    c.drawLine(center, center + lineRotation, _blue);
   }
 
   /// Released the alien to move in certain direction
   void release(Vector2 releaseVelocity) {
-    velocity = releaseVelocity;
-    isAttacking = true;
+    body.linearVelocity = releaseVelocity.normalized() * 100;
   }
 }
--- lib/game/game.dart
+++ lib/game/game.dart
@@ -1,16 +1,16 @@
 import 'dart:math';
 
-import 'package:flame/collisions.dart';
-import 'package:flame/game.dart';
+import 'package:flame/components.dart';
 import 'package:flame/input.dart';
+import 'package:flame_forge2d/flame_forge2d.dart';
 import 'package:flutter/material.dart';
 import 'package:multiplayer/game/alien.dart';
 
-class MyGame extends FlameGame with HasCollisionDetection, PanDetector {
+class MyGame extends Forge2DGame with HasCollisionDetection, PanDetector {
   MyGame({
     required this.onGameOver,
     required this.aliens,
-  });
+  }) : super(gravity: Vector2(0, 0), zoom: 10);
   final void Function() onGameOver;
 
   final List<Alien> aliens;
@@ -36,6 +36,7 @@ class MyGame extends FlameGame with HasCollisionDetection, PanDetector {
 
     _myAlien = aliens[0];
     addAll(aliens);
+    addAll(_createBoundaries(this));
   }
 
   late Vector2 _initialPanPosition;
@@ -57,5 +58,43 @@ class MyGame extends FlameGame with HasCollisionDetection, PanDetector {
   void onPanEnd(DragEndInfo info) {
     _myAlien.release(-_draggedDelta * 3);
     super.onPanEnd(info);
+  }
+}
+
+List<_Wall> _createBoundaries(Forge2DGame game) {
+  final topLeft = Vector2.zero();
+  final bottomRight = game.screenToWorld(game.camera.viewport.effectiveSize);
+  final topRight = Vector2(bottomRight.x, topLeft.y);
+  final bottomLeft = Vector2(topLeft.x, bottomRight.y);
+
+  return [
+    _Wall(topLeft, topRight),
+    _Wall(topRight, bottomRight),
+    _Wall(bottomRight, bottomLeft),
+    _Wall(bottomLeft, topLeft),
+  ];
+}
+
+class _Wall extends BodyComponent {
+  final Vector2 start;
+  final Vector2 end;
+
+  _Wall(this.start, this.end);
+
+  @override
+  Body createBody() {
+    final shape = EdgeShape()..set(start, end);
+    final fixtureDef = FixtureDef(
+      shape,
+      friction: 0.3,
+      restitution: 1,
+      density: 1000,
+    );
+    final bodyDef = BodyDef(
+      userData: this,
+      position: Vector2.zero(),
+    );
+
+    return world.createBody(bodyDef)..createFixture(fixtureDef);
   }
 }
